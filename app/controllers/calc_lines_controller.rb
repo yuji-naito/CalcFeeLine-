62f62a73b4115e7742978aa062a8a6b07e5536d9
EC2 Default User <ec2-user@ip-172-31-23-111.us-east-2.compute.internal>
2019-04-05 16:00:27 +0000

    Line@料金計算サイト一旦完成

--- app/controllers/calc_lines_controller.rb
+++ app/controllers/calc_lines_controller.rb
@@ -24,25 +24,49 @@ class CalcLinesController < ApplicationController
     ]
   
   def home
-    @line_histories = LineHistory.where(month: 1..12)
+    # 1～12月分を昇順に取得
+    @line_histories = LineHistory.where(month: 1..12).order(month: :asc)
+    @current_line = LineHistory.new
   end
   
   def calc
-    if params[:plan] == "free"
-      @fee = calc_free_plan(params[:traffic])
-    elsif params[:plan] == "light"
-      @fee = calc_light_plan(params[:traffic])
-    elsif params[:plan] == "standard"
-      @fee = calc_standard_plan(params[:traffic])
+    line_data = line_history_params
+    
+    # プランによって料金を計算する
+    if line_data[:plan] == "free"
+      @fee = calc_free_plan(line_data[:traffic].to_i)
+      if @fee == -1
+        flash[:notice] = "フリープランは1000通までです。"
+      end
+    elsif line_data[:plan] == "light"
+      @fee = calc_light_plan(line_data[:traffic].to_i)
+    elsif line_data[:plan] == "standard"
+      @fee = calc_standard_plan(line_data[:traffic].to_i)
     else
       # :planに想定外の値が入ってきた場合は固定で-1
       @fee = -1
+      flash[:notice] = "プランを選択してください。"
     end
     
-    @line_history = LineHistory.find_by(month: params[:month])
-    @line_history.update_attributes(fee: @fee, traffice: params[:traffic])
+    if (line_data[:month].to_i > 12) || (line_data[:month].to_i < 1)
+      @fee = -1
+      flash[:notice] = "月は1～12を入力してください"
+    end
     
-    @line_histories = LineHistory.where(month: 1..12)
+    # エラーが発生していなければ保存
+    if @fee != -1
+      # 入力した月のデータがあるかどうかを確認
+      @current_line = LineHistory.find_by(month: line_data[:month].to_i)
+      if @current_line.present?
+        @current_line.update_attributes!(fee: @fee, traffic: line_data[:traffic].to_i)
+      else
+        @current_line = LineHistory.new(fee: @fee, month: line_data[:month].to_i, traffic: line_data[:traffic].to_i)
+        @current_line.save!
+      end
+    end
+    
+    # 1～12月分を昇順に取得
+    @line_histories = LineHistory.where(month: 1..12).order(month: :asc)
   end
   
   private
@@ -74,22 +98,26 @@ class CalcLinesController < ApplicationController
     fee = STANDARD_PLAN_MONTH
     
     # 無料通信分を計算
-    temp_trf = traffice - STANDARD_PLAN_FREE_TRF
+    temp_trf = traffic - STANDARD_PLAN_FREE_TRF
     
     # 無料通信分以内であれば、追加なし
     return fee if temp_trf <= 0
     
     # 追加料金計算
     STANDARD_PLAN_FEE_PER_TRF.each do |one_fee|
-      if temp_trf > one_fee[0]
-        fee += one_fee[0] * one_fee[1]
-        temp_trf -= one_fee[0]
+      if temp_trf > one_fee[1]
+        fee += one_fee[1] * one_fee[0]
+        temp_trf -= one_fee[1]
       else
-        fee += temp_trf * one_fee[1]
+        fee += temp_trf * one_fee[0]
         break
       end
     end
     
     return fee
+  end
+  
+  def line_history_params
+    params.require(:line_history).permit(:month, :traffic, :plan)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   #get 'calc_lines/home'
 
   root 'calc_lines#home'
+  post 'calc_lines/calc'
 end
